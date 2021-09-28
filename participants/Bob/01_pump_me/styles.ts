e4da31fb319c9f580e38a1dd092497c68fea066b
Wojciech Bożek <bozek.wojciech@gmail.com>
2021-09-28 19:35:14 +0200

    Task1 bob

---
+++ participants/Bob/01_pump_me/styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  main: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+  },
+  pressable: {
+    borderRadius: 40,
+    backgroundColor: "pink",
+    padding: 10,
+  },
+  textOnBaloon: {
+    fontSize: 20,
+    color: "white",
+  },
+});
