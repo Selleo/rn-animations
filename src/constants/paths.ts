f07d6e2f4e8a90c39c7a65795f88458fada4ae53
Tomasz Kielar <32620457+k1eu@users.noreply.github.com>
2021-11-07 02:09:52 +0100

    fix: paths directory (#12)
    * fix: paths directory
    
    * fix: no gosia
    
    * fix: gosia back

--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -4,28 +4,28 @@ export const paths: Paths = {
   bb: [
     {
       name: "Pump me",
-      component: lazy(() => import("@participants/BB/01_pump_me")),
+      component: lazy(() => import("@participants/bb/01_pump_me")),
     },
     {
       name: "Tomato thrower",
-      component: lazy(() => import("@participants/BB/02_tomato_thrower")),
+      component: lazy(() => import("@participants/bb/02_tomato_thrower")),
     },
   ],
   bob: [
     {
       name: "01_pump_me",
-      component: lazy(() => import("@participants/Bob/01_pump_me")),
+      component: lazy(() => import("@participants/bob/01_pump_me")),
     },
   ],
   elkubok: [],
   gosia: [
     {
       name: "01_pump_me",
-      component: lazy(() => import("@participants/Gosia/01_pump_me")),
+      component: lazy(() => import("@participants/gosia/01_pump_me")),
     },
     {
       name: "02_tomato_catapult",
-      component: lazy(() => import("@participants/Gosia/02_tomato_catapult")),
+      component: lazy(() => import("@participants/gosia/02_tomato_catapult")),
     },
   ],
   k1eu: [
