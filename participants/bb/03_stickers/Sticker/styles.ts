81302448572b5527a1034e99af66fa54305b66e4
bartoszboruta <bboruta.dev@gmail.com>
2021-11-07 19:36:07 +0100

    add ability to make sticker go back to default position on click

--- participants/bb/03_stickers/Sticker/styles.ts
+++ participants/bb/03_stickers/Sticker/styles.ts
@@ -5,14 +5,24 @@ export const STICKER_HEIGHT = 60;
 const styles = StyleSheet.create({
   image: {
     height: STICKER_HEIGHT,
-    width: STICKER_HEIGHT,
     shadowColor: "#000",
     shadowOffset: {
-      width: 2,
       height: 2,
+      width: 2,
     },
-
     shadowRadius: 3.0,
+    width: STICKER_HEIGHT,
+  },
+  imagePlaceholder: {
+    opacity: 0.2,
+  },
+  placeholderContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    position: "absolute",
+  },
+  placeholderIcon: {
+    position: "absolute",
   },
   stickerContainer: {
     borderColor: "gray",
