44c4a53f2833e279cdf7ca47143b6a64f2cc1f0b
bartoszboruta <bboruta.dev@gmail.com>
2021-12-20 23:07:05 +0100

    add shared transition animation

---
+++ participants/bb/05_shared_transition/components/Rate/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "flex-end",
+    flexDirection: "row",
+  },
+  icon: {
+    marginLeft: 5,
+  },
+});
+
+export default styles;
