3fc0017f7f45b7785972a21ab39e2f7b0eede627
Tomasz Kielar <git@k1eu.dev>
2022-03-10 19:51:06 +0100

    wip

--- participants/k1eu/08_liquid/Liquid.tsx
+++ participants/k1eu/08_liquid/Liquid.tsx
@@ -1,43 +1,65 @@
 import { useState } from "react";
-import { View, Text, Pressable, StyleSheet } from "react-native";
+import { View, Text, Pressable, StyleSheet, Image, Dimensions } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import Animated, {
+  FadeInRight,
+  FadeOutLeft,
   interpolate,
   interpolateColor,
-  interpolateColors,
-  interpolateNode,
   runOnJS,
-  useAnimatedRef,
+  SlideInRight,
+  SlideOutLeft,
   useAnimatedStyle,
   useSharedValue,
   withTiming,
+  ZoomInRight,
+  ZoomOutLeft,
 } from "react-native-reanimated";
 
 const pink = "#FAAFD4";
 const white = "#FFFFFF";
 const blue = "#012BC4";
+const black = "#000000";
+
+const {  width } = Dimensions.get("window");
+
+const data = [
+  {
+    id: 1,
+    title: 'Drag and drop to move',
+    img: require('./assets/1.png'),
+    fontColor: black,
+  },
+  {
+    id: 2,
+    title: 'Local news stories',
+    img: require('./assets/2.png'),
+    fontColor: white,
+  },
+  {
+    id: 3,
+    title: 'Choose your interests',
+    img: require('./assets/3.png'),
+    fontColor: white,
+  },
+]
 
 const colors = [white, pink, blue];
 const pages = colors.length;
 
-const Button = ({ disabled, screenState, onPress }) => {
-  const valueSteps = [...Array.from({ length: pages * 2 }, (_, i) => i * 0.5)];
-  const colorSteps = colors.reduce((acc, color) => {
-    return [...acc, color, color];
-  }, []) 
-  console.log(colorSteps)
+const Button = ({ disabled, index, screenState, onPress }) => {
   const backgroundStyles = useAnimatedStyle(() => ({
     backgroundColor: interpolateColor(
       screenState.value,
-      [0, 0.5, 1, 1.5, 2],
-      [white, white, pink, pink, blue, blue]
+      [0, 0.5, 0.5001, 1, 1.5, 1.5001, 2],
+      [white, white, blue, blue, blue, pink, pink]
     ),
   }));
   const buttonStyles = useAnimatedStyle(() => ({
     backgroundColor: interpolateColor(
       screenState.value,
-      [0, 0.5, 1],
-      [pink, pink, white]
+      [0, 0.5, 0.5001, 0.9, 1, 1.5, 1.5001, 1.9, 2],
+      [blue, blue, white, white, pink, pink, blue, blue, white]
     ),
     transform: [
       {
@@ -46,18 +68,54 @@ const Button = ({ disabled, screenState, onPress }) => {
       {
         rotateY: `${interpolate(
           screenState.value,
-          [0, 0.5, 1],
-          [0, -90, -180]
+          [0, 0.5, 1, 1.5, 2],
+          [0, -90, -180, -270, -360]
         )}deg`,
       },
       {
-        scale: interpolate(screenState.value, [0, 0.5, 1], [1, 6, 1]),
+        scale: interpolate(
+          screenState.value,
+          [0, 0.5, 1, 1.5, 2],
+          [1, 6, 1, 6, 1]
+        ),
       },
-      // {
-      //   translateX: '20%',
-      // }
     ],
   }));
+  const textStyles = useAnimatedStyle(() => ({
+    color: interpolateColor(
+      screenState.value,
+      [0, 0.5, 0.5001, 2],
+      [white, white, black, black]
+    ),
+    transform: [
+      {
+        rotateY: `${interpolate(
+          screenState.value,
+          [0, 0.5, 0,501, 1, 1.5, 1.501, 2],
+          [0, 0, 0, 0, 0, 0, 0]
+        )}deg`,
+      },
+    ],
+  }));
+
+  const renderCorrectChuj = () => {
+    const item = data[index]
+    return (
+      <Animated.View
+        entering={SlideInRight.delay(750)}
+        exiting={SlideOutLeft.duration(750)}
+        key={index}
+        style={{
+          marginBottom: 100,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Image style={{ width: width * 0.7 }} source={item.img}/>
+        <Text style={{fontSize: 40, width: width * 0.8, textAlign: 'center', color: item.fontColor}}>{item.title}</Text>
+      </Animated.View>
+    );
+  };
 
   return (
     <Animated.View
@@ -67,13 +125,13 @@ const Button = ({ disabled, screenState, onPress }) => {
         backgroundStyles,
       ]}
     >
+      {renderCorrectChuj()}
       <Animated.View style={[styles.button, buttonStyles]}>
         <TouchableOpacity
           style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-          disabled={disabled}
           onPress={onPress}
         >
-          <Text>xd</Text>
+          <Animated.Text style={[{ fontSize: 30 },textStyles]}>{'>'}</Animated.Text>
         </TouchableOpacity>
       </Animated.View>
     </Animated.View>
@@ -92,18 +150,17 @@ const Liquid = () => {
 
   const onPress = () => {
     // screenState.value = Number(!Boolean(screenState.value));
-    const toValue = screenState.value === 0 ? 1 : 0;
-    screenState.value = withTiming(
-      toValue,
-      {
-        duration: 1000,
-      },
-      (finished) => {
-        if (finished) {
-          runOnJS(increaseIndex)();
-        }
-      }
-    );
+    let toValue: number;
+    if (screenState.value < pages - 1) {
+      toValue = screenState.value + 1;
+    } else {
+      toValue = 0;
+    }
+    console.log(toValue);
+    runOnJS(increaseIndex)();
+    screenState.value = withTiming(toValue, {
+      duration: 1500,
+    });
   };
 
   return (
@@ -111,7 +168,9 @@ const Liquid = () => {
       <Button
         onPress={onPress}
         screenState={screenState}
-        disabled={index === 2}
+        disabled={index === pages - 1}
+        index={index}
+        // disabled={false}
       />
     </View>
   );
